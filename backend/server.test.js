de5b4b03d46b631dc0b5ef2db21c0c81568be089
Keerthivasan B <Keerthivasanb@canvendor.com>
2025-03-27 12:17:44 +0530

    created server.test.js

---
+++ backend/server.test.js
@@ -0,0 +1,6 @@
+const request = require("supertest");
+const app = require("../server"); // Adjust the path based on your `server.js` setup
+
+test("Sample test", () => {
+    expect(1 + 1).toBe(2);
+});
